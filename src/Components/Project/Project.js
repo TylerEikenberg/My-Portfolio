a031f0ed8ff3ec4f7d1ddd894bf857cfdc5548e8
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-02 14:18:39 -0500

    working on proj styling

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -20,10 +20,13 @@ class Project extends Component {
 
   //TODO:
   //FIGURE OUT HOW TO MAKE IMAGE GROW SLIGHTLY ON HOVER
+  //on hover hide name, show description and button linkss
   render() {
     let overlayClasses = "project-box-overlay hidden";
+    let hidethis = "hidden-words";
     if (!this.state.hover) {
       overlayClasses = "project-box-overlay";
+      hidethis = "show-title";
     }
     if (this.props.galaxy) {
       return (
@@ -33,7 +36,13 @@ class Project extends Component {
             onMouseEnter={this.isMouseEnter}
             onMouseLeave={this.onMouseLeave}
           ></div>
-          <h1>Galaxy Dawg</h1>
+          <h1 className={hidethis}>Galaxy Dawg</h1>
+          <h4>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            provident iste libero sunt quae explicabo corporis magni accusantium
+            amet natus nulla culpa tempore, quisquam atque voluptates,
+            architecto omnis tempora ab.
+          </h4>
           <img src={phi} alt="whoops"></img>
         </div>
       );
