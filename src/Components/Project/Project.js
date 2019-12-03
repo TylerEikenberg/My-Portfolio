e42a7f7ca0580ec73ea2391373a12fd674eb3876
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-02 21:35:56 -0500

    adding styling and info

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./Project.css";
-import galaxydawg from "../../Assets/galaxydawg.jpg";
 
 class Project extends Component {
   constructor() {
@@ -38,12 +37,12 @@ class Project extends Component {
             <h1 className={hidethis}>Galaxy Dawg</h1>
           </div>
           <div className="about-project-container">
-            <h4>
-              Galaxy Dawg is a top down shoot-em-up style single player game
-              playable on desktop and mobile devices. Inspired by classic games
-              like "Space Invaders" and "Raiden"; Galaxy Dawg allows you to play
-              as ace space pilot "Commander Pup" in his mission to protect his
-              home planet from alien invaders.
+            <h4 className="about-span">
+              <span>Galaxy Dawg</span> is a top down shoot-em-up style single
+              player game playable on desktop and mobile devices. Inspired by
+              classic games like "Space Invaders" and "Raiden"; Galaxy Dawg
+              allows you to play as ace space pilot "Commander Pup" in his
+              mission to protect his home planet from alien invaders.
             </h4>
           </div>
         </div>
@@ -51,13 +50,24 @@ class Project extends Component {
     }
     if (this.props.delicious) {
       return (
-        <div className="project-box-container">
-          <div
-            onMouseEnter={this.isMouseEnter}
-            onMouseLeave={this.onMouseLeave}
-          ></div>
-          <h1>DeliCious</h1>
-          <img src={galaxydawg} alt="whoops"></img>
+        <div className="project-container">
+          <div className="project-box-container delicious">
+            <div
+              className="project-box-overlay delicious-overlay"
+              onMouseEnter={this.isMouseEnter}
+              onMouseLeave={this.onMouseLeave}
+            ></div>
+            <h1 className={hidethis}>DeliCious</h1>
+          </div>
+          <div className="about-project-container">
+            <h4 className="about-span">
+              <span>DeliCious</span> is a top down shoot-em-up style single
+              player game playable on desktop and mobile devices. Inspired by
+              classic games like "Space Invaders" and "Raiden"; Galaxy Dawg
+              allows you to play as ace space pilot "Commander Pup" in his
+              mission to protect his home planet from alien invaders.
+            </h4>
+          </div>
         </div>
       );
     }
